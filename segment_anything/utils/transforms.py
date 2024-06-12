7b6b7f1c7369821934976e6aeb1c0d0fc25a3df9
cile98 <jovan.cicvaric@yahoo.com>
2024-06-12 15:55:07 +0200

    resizing changes

--- segment_anything/utils/transforms.py
+++ segment_anything/utils/transforms.py
@@ -27,24 +27,39 @@ class ResizeLongestSide:
         """
         Expects a numpy array with shape HxWxC in uint8 format.
         """
-        target_size = self.get_preprocess_shape(image.shape[0], image.shape[1], self.target_length)
-        return np.array(resize(to_pil_image(image), target_size))
+        target_size = self.get_preprocess_shape(
+            image.shape[0],
+            image.shape[1],
+            self.target_length,
+        )
+        resized_image = resize(image.permute(2, 0, 1), target_size)
+        return resized_image
 
-    def apply_coords(self, coords: np.ndarray, original_size: Tuple[int, ...]) -> np.ndarray:
+    def apply_coords(
+        self,
+        coords: np.ndarray,
+        original_size: Tuple[int, ...],
+    ) -> np.ndarray:
         """
         Expects a numpy array of length 2 in the final dimension. Requires the
         original image size in (H, W) format.
         """
         old_h, old_w = original_size
         new_h, new_w = self.get_preprocess_shape(
-            original_size[0], original_size[1], self.target_length
+            original_size[0],
+            original_size[1],
+            self.target_length,
         )
         coords = deepcopy(coords).astype(float)
         coords[..., 0] = coords[..., 0] * (new_w / old_w)
         coords[..., 1] = coords[..., 1] * (new_h / old_h)
         return coords
 
-    def apply_boxes(self, boxes: np.ndarray, original_size: Tuple[int, ...]) -> np.ndarray:
+    def apply_boxes(
+        self,
+        boxes: np.ndarray,
+        original_size: Tuple[int, ...],
+    ) -> np.ndarray:
         """
         Expects a numpy array shape Bx4. Requires the original image size
         in (H, W) format.
@@ -59,13 +74,23 @@ class ResizeLongestSide:
         the transformation expected by the model.
         """
         # Expects an image in BCHW format. May not exactly match apply_image.
-        target_size = self.get_preprocess_shape(image.shape[2], image.shape[3], self.target_length)
+        target_size = self.get_preprocess_shape(
+            image.shape[2],
+            image.shape[3],
+            self.target_length,
+        )
         return F.interpolate(
-            image, target_size, mode="bilinear", align_corners=False, antialias=True
+            image,
+            target_size,
+            mode="bilinear",
+            align_corners=False,
+            antialias=True,
         )
 
     def apply_coords_torch(
-        self, coords: torch.Tensor, original_size: Tuple[int, ...]
+        self,
+        coords: torch.Tensor,
+        original_size: Tuple[int, ...],
     ) -> torch.Tensor:
         """
         Expects a torch tensor with length 2 in the last dimension. Requires the
@@ -73,7 +98,9 @@ class ResizeLongestSide:
         """
         old_h, old_w = original_size
         new_h, new_w = self.get_preprocess_shape(
-            original_size[0], original_size[1], self.target_length
+            original_size[0],
+            original_size[1],
+            self.target_length,
         )
         coords = deepcopy(coords).to(torch.float)
         coords[..., 0] = coords[..., 0] * (new_w / old_w)
@@ -81,7 +108,9 @@ class ResizeLongestSide:
         return coords
 
     def apply_boxes_torch(
-        self, boxes: torch.Tensor, original_size: Tuple[int, ...]
+        self,
+        boxes: torch.Tensor,
+        original_size: Tuple[int, ...],
     ) -> torch.Tensor:
         """
         Expects a torch tensor with shape Bx4. Requires the original image
@@ -91,7 +120,11 @@ class ResizeLongestSide:
         return boxes.reshape(-1, 4)
 
     @staticmethod
-    def get_preprocess_shape(oldh: int, oldw: int, long_side_length: int) -> Tuple[int, int]:
+    def get_preprocess_shape(
+        oldh: int,
+        oldw: int,
+        long_side_length: int,
+    ) -> Tuple[int, int]:
         """
         Compute the output size given input size and target long side length.
         """
