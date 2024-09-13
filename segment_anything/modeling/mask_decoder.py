9daa4aecd305b37cf6769fc19c15955a8779c9b7
cile98 <jovan.cicvaric@yahoo.com>
2024-09-13 11:30:36 +0000

    small change to enable proper tracing

--- segment_anything/modeling/mask_decoder.py
+++ segment_anything/modeling/mask_decoder.py
@@ -123,9 +123,9 @@ class MaskDecoder(nn.Module):
         tokens = torch.cat((output_tokens, sparse_prompt_embeddings), dim=1)
 
         # Expand per-image data in batch direction to be per-mask
-        src = torch.repeat_interleave(image_embeddings, tokens.shape[0], dim=0)
+        src = torch.repeat_interleave(image_embeddings, torch.tensor(tokens.shape[0], device=image_embeddings.device), dim=0)
         src = src + dense_prompt_embeddings
-        pos_src = torch.repeat_interleave(image_pe, tokens.shape[0], dim=0)
+        pos_src = torch.repeat_interleave(image_pe, torch.tensor(tokens.shape[0], device=image_embeddings.device), dim=0)
         b, c, h, w = src.shape
 
         # Run the transformer
